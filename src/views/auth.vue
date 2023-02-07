c6565fd0cd8870dfdd69bcaf7c17a203aa9ef0dc
leagrave <lea.grave@orange.fr>
2023-02-07 11:48:23 +0100

    route auth

--- src/views/auth.vue
+++ src/views/auth.vue
@@ -6,7 +6,7 @@
             <input type="text" id="name" name="name">
         </div>
         
-        <router-link to="/Home"><button type="submit">Sign Up</button></router-link>
+        <router-link to="/Home"><button type="submit" @click="valideGroupe">Sign Up</button></router-link>
     </form>
 </template>
 
@@ -18,6 +18,32 @@ import Home from './Home.vue';
 export default defineComponent({
   name: 'auth',
   components: { Home },
+  methodes: {
+    methModiStyle(){
+      app.get("/signup", (req, res) => {
+  if (!req.query.username) {
+      return res.status(400).json({ message: 'Veuillez entrer un nom de groupe' })
+  }
+
+  const usrs = require("./users.json");
+  const user = usrs.find(u => u.username === req.query.username)
+
+  if (user) {
+      return res.status(400).json({ message: 'Utilisateur existe déjà' })
+  }
+
+  let file = editJsonFile('./users.json');
+  var user_data = { "username": req.query.username, "points": 0 };
+  file.append("", user_data);
+  file.save();
+
+  return res.json({ message: 'Utilisateur créé' });
+
+ 
+});
+
+    }
+  }
  
 });
 
